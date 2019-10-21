1766a8e58f5486f21c4470e83426c80f2434aacc
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-21 00:07:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,55 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+
+def nyc_pigeon_organizer (data)
+  final = {}
+data.each do |first_level, all_other|
+    all_other.each do |category, array|
+      array.each do |name|
+        final[name] = {:color => [], :gender => [], :lives => []}
+      end 
+    end 
+  end 
+  x = final.keys
+  data[:color].each do |bird_color, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:color] << bird_color.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:gender].each do |gender, type|
+    type.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:gender] << gender.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:lives].each do |location, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:lives] << location
+        end 
+      end 
+    end 
+  end 
+  
+  return final 
+end 
+
+
+
+
+
+#key are color,gender,lives,values are another hash 
+#within each hash keys are colors,genders,locations
+#every hash  has array which all pigeons names in it.
+
+
+
+#newhash, keys are pigeons names 
+#values are another hash key are color,gender,lives
+#values going to another array with containing data 
